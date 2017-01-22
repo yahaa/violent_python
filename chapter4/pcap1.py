81d0ab889203fbe28fc56e7f1f372d9ed31981d7
yahaa <1477765176@qq.com>
2017-01-22 18:11:47 +0800

    在Google 地球上画流量去向线

--- chapter4/pcap1.py
+++ chapter4/pcap1.py
@@ -5,42 +5,41 @@ gi = pygeoip.GeoIP('Geo.dat')
 
 
 def capPack(pc):
-    kmlPts = ''
+    kmlLts = ''
     ct = 0
     for ptime, pdata in pc:
         try:
-            if ct >= 100:
+            if ct >= 10:
                 break
             p = dpkt.ethernet.Ethernet(pdata)
             if p.data.__class__.__name__ == 'IP':
                 ipsrc = '%d.%d.%d.%d' % tuple(map(ord, list(p.data.src)))
                 ipdst = '%d.%d.%d.%d' % tuple(map(ord, list(p.data.dst)))
-                srckml = retKML(ipsrc)
-                dstkml = retKML(ipdst)
-                kmlPts = kmlPts + srckml + dstkml
+                kmlLts = kmlLts + retKML(ipsrc, ipdst)
                 ct += 1
-        # src = getDetail(str(ipsrc))
-        # dst = getDetail(str(ipdst))
-        # print ipsrc, ' to ', ipdst
-        # print src, ' to ', dst, '\n\n
+
         except Exception, e:
             pass
-    return kmlPts
+    return kmlLts
 
 
-def retKML(ip):
-    rec = gi.record_by_name(ip)
+def retKML(ip1, ip2):
+    rec1 = gi.record_by_name(ip1)
+    rec2 = gi.record_by_name(ip2)
     try:
-        longitude = rec['longitude']
-        latitude = rec['latitude']
+        longitude1 = rec1['longitude']
+        latitude1 = rec1['latitude']
+        longitude2 = rec2['longitude']
+        latitude2 = rec2['latitude']
         kml = (
             '<Placemark>\n'
-            '<name>%s</name>\n'
-            '<Point>\n'
-            '<coordinates>%6f,%6f</coordinates>\n'
-            '</Point>\n'
+            '<name>%s to %s</name>\n'
+            '<LineString>\n'
+            '<coordinates>%6f,%6f,%6f,%6f</coordinates>\n'
+            '</LineString>\n'
             '</Placemark>\n'
-        ) % (ip, longitude, latitude)
+        ) % (ip1, ip2, longitude1, latitude1, longitude2, latitude2)
+
         return kml
     except Exception, e:
         return ''
@@ -64,10 +63,10 @@ def main():
     pc = pcap.pcap('wlan0')
     pc.setfilter('tcp port 80')
     kmlheader = '<?xml version="1.0" encoding="UTF-8"?>'\
-    '\n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n'
+        '\n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n'
     kmlfooter = '</Document>\n</kml>\n'
     print kmlheader + capPack(pc) + kmlfooter
 
 if __name__ == '__main__':
-        # print retKML('115.29.146.79')
+    # print retKML('115.29.146.79', '115.29.146.220')
     main()
