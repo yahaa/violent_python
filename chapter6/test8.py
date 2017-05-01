c9b7bfc92cde7cc41cc6739b86f907cfb38b7105
zihua <1477765176@qq.com>
2017-05-01 16:27:06 +0800

    add

--- chapter6/test8.py
+++ chapter6/test8.py
@@ -1,5 +1,5 @@
-
-s = 'zihua:$6$6l9AIe.Z$vYJG66jLd/EgQyGeht.6BApgbOQW.Phz6FrCrN2Snwfll.L/vhNcAi2C4ym8V0CJtu9IiKrv/SekTkS/K7dJG/:17111:0:99999:7:::'
-for item in s.split(':'):
-    print item
-    print ttt
+import os
+import sklearn
+for i in range(100):
+    i = i + i
+    print i
